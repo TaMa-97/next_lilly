65d5e9dbd640bc244239b1f1bc3d7fafb7c19f10
myk19977 <myk19977@gmail.com>
2023-04-04 00:14:23 +0900

    add: markdown機能追加_TOPページに記事一覧を表示

--- lilly/src/pages/index.tsx
+++ lilly/src/pages/index.tsx
@@ -14,17 +14,17 @@ export const getStaticProps = async () => {
 const Home: NextPage<Props> = ({ allPosts }) => (
   <ul>
     {allPosts?.map((post) => (
-      <a key={post.slug} mt={10}>
-        <li>
-          <h2>{post.title}</h2>
-          <p>{post.date}</p>
-          <ul>
-            {post.tags?.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </ul>
-        </li>
-      </a>
+      <li key={post.slug}>
+        <a href={`/blog/${post.slug}`}>
+          <p>{post.title}</p>
+        </a>
+        <p>{post.date}</p>
+        <ul>
+          {post.tags?.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </li>
     ))}
   </ul>
 )
