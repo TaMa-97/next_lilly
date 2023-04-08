8f2f090526a0ec6262cf80bea2315cd6b4aee566
myk19977 <myk19977@gmail.com>
2023-04-08 18:03:49 +0900

    style: font追加 - NotoSanJP 400,500,700 - Roboto 400,500Italic,700

--- lilly/src/pages/_document.tsx
+++ lilly/src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Roboto:wght@400;500i;700&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
