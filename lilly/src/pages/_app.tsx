9c8c599ce049e683146a032e57771a1750991d9d
myk19977 <myk19977@gmail.com>
2023-04-16 20:34:21 +0900

    add: Tocbot CSSファイルインポート #4

--- lilly/src/pages/_app.tsx
+++ lilly/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 import '@/styles/globals.scss'
 import 'zenn-content-css'
+import 'tocbot/dist/tocbot.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
