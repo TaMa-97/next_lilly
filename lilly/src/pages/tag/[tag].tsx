f27921aaee75250e74083e188c8e6d7e1c18131f
myk19977 <myk19977@gmail.com>
2023-04-23 23:52:02 +0900

    add: カテゴリーページ作成  #3

--- lilly/src/pages/tag/[tag].tsx
+++ lilly/src/pages/tag/[tag].tsx
@@ -1,8 +1,14 @@
 import type { GetStaticPaths, GetStaticProps } from 'next'
 import type { Post } from '@/utils/api'
 import React from 'react'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { getAllPosts } from '@/utils/api'
+import CustomHead from '@/components/base/Head/CustomHead'
+import Header from '@/components/base/Header/Header'
+import Footer from '@/components/base/Footer/Footer'
+import styles from './index.module.scss'
 
 // TagPage コンポーネントのpropsを定義
 type TagPageProps = {
@@ -15,17 +21,69 @@ const TagPage: React.FC<TagPageProps> = ({ posts }) => {
   const router = useRouter()
   const { tag } = router.query
 
+  const pageTitle = 'Lilly'
+  const pageDescription =
+    'Lillyの個人サイトです。This is the Home page of Lilly'
+
   // タグと関連する投稿を表示
   return (
     <>
-      <h1>Tag: {tag}</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <a href={`/blog/${post.slug}`}>{post.title}</a>
-          </li>
-        ))}
-      </ul>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: 'easeOut', duration: 0.25 }}
+        className="modWrapper"
+      >
+        <CustomHead title={pageTitle} description={pageDescription} />
+        <Header />
+        <main>
+          <section className={styles.myTag}>
+            <div className="container">
+              <motion.div
+                className={styles.myTag__head}
+                initial={{ y: 15, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -15, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <h2 className={styles.myTag__title}>
+                  <span className={styles.myTag__titleIcon}>Category</span>
+                  {tag}
+                </h2>
+              </motion.div>
+
+              <ul className={styles.myTag__list}>
+                {posts.map((post) => (
+                  <li key={post.slug} className={styles.myTag__item}>
+                    <Link
+                      href={`/blog/${post.slug}`}
+                      className={styles.myTag__link}
+                      scroll={false}
+                    >
+                      <p className={styles.myTag__linkTitle}>
+                        {post.title}
+                        <span className={styles.myTag__linkIcon}></span>
+                      </p>
+                      <div className={styles.myTag__linkInner}>
+                        <ul className={styles.myTag__linkList}>
+                          {post.tags?.map((tag) => (
+                            <li key={tag} className={styles.myTag__linkItem}>
+                              {tag}
+                            </li>
+                          ))}
+                        </ul>
+                        <p className={styles.myTag__linkDate}>{post.date}</p>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </motion.div>
     </>
   )
 }
