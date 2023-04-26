6630460861b6491f3d955a2fd9ed473741006ef3
myk19977 <myk19977@gmail.com>
2023-04-27 00:13:05 +0900

    update: categoryタイトルが一瞬消える、ローディングスピナー表示

--- lilly/src/pages/category/[category].tsx
+++ lilly/src/pages/category/[category].tsx
@@ -8,6 +8,7 @@ import { getAllPosts } from '@/utils/api'
 import CustomHead from '@/components/base/Head/CustomHead'
 import Header from '@/components/base/Header/Header'
 import Footer from '@/components/base/Footer/Footer'
+import LoadingSpinner from '@/components/elements/LoadingSpinner/LoadingSpinner'
 import styles from './index.module.scss'
 
 // TagPage コンポーネントのpropsを定義
@@ -28,62 +29,66 @@ const TagPage: React.FC<TagPageProps> = ({ posts }) => {
   // タグと関連する投稿を表示
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ ease: 'easeOut', duration: 0.25 }}
-        className="modWrapper"
-      >
-        <CustomHead title={pageTitle} description={pageDescription} />
-        <Header />
-        <main>
-          <section className={styles.myTag}>
-            <div className="container">
-              <motion.div
-                className={styles.myTag__head}
-                initial={{ y: 15, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: -15, opacity: 0 }}
-                transition={{ duration: 0.2 }}
-              >
-                <h2 className={styles.myTag__title}>
-                  <span className={styles.myTag__titleIcon}>Category</span>
-                  {category}
-                </h2>
-              </motion.div>
+      {category ? (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ ease: 'easeOut', duration: 0.25 }}
+          className="modWrapper"
+        >
+          <CustomHead title={pageTitle} description={pageDescription} />
+          <Header />
+          <main>
+            <section className={styles.myTag}>
+              <div className="container">
+                <motion.div
+                  className={styles.myTag__head}
+                  initial={{ y: 15, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: -15, opacity: 0 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <h2 className={styles.myTag__title}>
+                    <span className={styles.myTag__titleIcon}>Category</span>
+                    {category}
+                  </h2>
+                </motion.div>
 
-              <ul className={styles.myTag__list}>
-                {posts.map((post) => (
-                  <li key={post.slug} className={styles.myTag__item}>
-                    <Link
-                      href={`/blog/${post.slug}`}
-                      className={styles.myTag__link}
-                      scroll={false}
-                    >
-                      <p className={styles.myTag__linkTitle}>
-                        {post.title}
-                        <span className={styles.myTag__linkIcon}></span>
-                      </p>
-                      <div className={styles.myTag__linkInner}>
-                        <ul className={styles.myTag__linkList}>
-                          {post.tags?.map((tag) => (
-                            <li key={tag} className={styles.myTag__linkItem}>
-                              {tag}
-                            </li>
-                          ))}
-                        </ul>
-                        <p className={styles.myTag__linkDate}>{post.date}</p>
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-        </main>
-        <Footer />
-      </motion.div>
+                <ul className={styles.myTag__list}>
+                  {posts.map((post) => (
+                    <li key={post.slug} className={styles.myTag__item}>
+                      <Link
+                        href={`/blog/${post.slug}`}
+                        className={styles.myTag__link}
+                        scroll={false}
+                      >
+                        <p className={styles.myTag__linkTitle}>
+                          {post.title}
+                          <span className={styles.myTag__linkIcon}></span>
+                        </p>
+                        <div className={styles.myTag__linkInner}>
+                          <ul className={styles.myTag__linkList}>
+                            {post.tags?.map((tag) => (
+                              <li key={tag} className={styles.myTag__linkItem}>
+                                {tag}
+                              </li>
+                            ))}
+                          </ul>
+                          <p className={styles.myTag__linkDate}>{post.date}</p>
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+          </main>
+          <Footer />
+        </motion.div>
+      ) : (
+        <LoadingSpinner />
+      )}
     </>
   )
 }
