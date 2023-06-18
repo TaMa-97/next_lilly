4139adf63bc0181dd8850ed39e13886b97688d93
miyake <myk19977@gmail.com>
2023-06-18 18:14:21 +0900

    add: ThreeBackground components

---
+++ lilly/src/components/elements/ThreeBackground/ThreeBackground.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+import FOG from 'vanta/dist/vanta.fog.min'
+
+function ThreeBackground() {
+  const myRef = useRef()
+
+  useEffect(() => {
+    let vantaEffect
+    if (myRef.current) {
+      vantaEffect = FOG({
+        el: myRef.current,
+        THREE,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.0,
+        minWidth: 200.0,
+        highlightColor: 0xceffe8,
+        midtoneColor: 0xffd6e3,
+        lowlightColor: 0x67b2ff,
+        baseColor: 0xffffff,
+        blurFactor: 0.88,
+        speed: 3.8,
+        zoom: 0.4,
+      })
+    }
+
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [])
+
+  return (
+    <div
+      ref={myRef}
+      style={{
+        position: 'fixed',
+        width: '100%',
+        height: '100vh',
+        top: 0,
+        left: 0,
+        zIndex: -1,
+      }}
+    />
+  )
+}
+
+export default ThreeBackground
--- lilly/src/pages/_app.tsx
+++ lilly/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 import { AnimatePresence } from 'framer-motion'
 import { useNextCssRemovalPrevention } from '@/hooks/useNextCssRemovalPrevention'
+import ThreeBackground from '@/components/elements/ThreeBackground/ThreeBackground'
 import '@/styles/globals.scss'
 import 'zenn-content-css'
 import 'tocbot/dist/tocbot.css'
@@ -10,14 +11,17 @@ export default function App({ Component, pageProps, router }: AppProps) {
   useNextCssRemovalPrevention()
 
   return (
-    <AnimatePresence
-      initial={false}
-      mode={'wait'}
-      onExitComplete={() => {
-        window.scrollTo(0, 0)
-      }}
-    >
-      <Component key={router.asPath} {...pageProps} />
-    </AnimatePresence>
+    <>
+      <ThreeBackground />
+      <AnimatePresence
+        initial={false}
+        mode={'wait'}
+        onExitComplete={() => {
+          window.scrollTo(0, 0)
+        }}
+      >
+        <Component key={router.asPath} {...pageProps} />
+      </AnimatePresence>
+    </>
   )
 }
