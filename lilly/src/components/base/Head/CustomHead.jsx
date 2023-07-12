ce47c45d795b1b3146daccf5a0c9dd9ea3db6133
miyake <myk19977@gmail.com>
2023-07-12 19:58:24 +0900

    ✨ ogimage追加

--- lilly/src/components/base/Head/CustomHead.jsx
+++ lilly/src/components/base/Head/CustomHead.jsx
@@ -15,13 +15,13 @@ const CustomHead = ({ title, description }) => {
       <meta name="format-detection" content="telephone=no" />
       <meta property="og:locale" content="ja_JP" />
       <meta property="og:site_name" content="Lilly" />
-      <meta property="og:image" content="/ogimage.png" />
+      <meta property="og:image" content="https://lilly-dog.dev/ogimage.jpg" />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:url" content={canonicalURL} />
       <link rel="canonical" href={canonicalURL} />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="https://lilly-dog.dev/favicon.ico" />
     </Head>
   )
 }
