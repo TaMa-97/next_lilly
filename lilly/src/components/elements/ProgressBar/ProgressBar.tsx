8d7476b0a6a19f72479423cfd7010205e7d94966
miyake <myk19977@gmail.com>
2024-03-11 21:21:57 +0900

    ♻️ refactoring

--- lilly/src/components/elements/ProgressBar/ProgressBar.tsx
+++ lilly/src/components/elements/ProgressBar/ProgressBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { motion, useScroll, useSpring } from 'framer-motion'
+import styles from './ProgressBar.module.scss'
 
 const ProgressBar = () => {
   const { scrollYProgress } = useScroll()
   const scaleX = useSpring(scrollYProgress)
 
-  return <motion.div style={{ scaleX }} className="progressBar" />
+  return <motion.div style={{ scaleX }} className={styles.progressBar} />
 }
 
 export default ProgressBar
